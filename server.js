ecc0466713707a5f6d1fea16a2fa0dcf8d80f6ba
Araceligm90 <garcia.araceli90@gmail.com>
2023-03-01 00:15:34 -0500

    basics of backend added

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 3002;
+
+// const findPets = require('./modules/pets');
+
+
+app.get('/', (req, res) => {
+  res.send('Hola. Your default endpoint is working');
+});
+
+// app.get('/pets', findPets);
+
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
