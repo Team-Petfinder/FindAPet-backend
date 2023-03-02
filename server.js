070d29bb149eebbfc78130159aae9120bb166078
Araceligm90 <garcia.araceli90@gmail.com>
2023-03-02 00:15:02 -0500

    get function to working

--- server.js
+++ server.js
@@ -3,21 +3,34 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const handlePets = require('./modules/pets');
 
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
 
 const PORT = process.env.PORT || 3002;
 
-// const findPets = require('./modules/pets');
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGODB_URL);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => console.log('Connected to Mongoose'));
+
 
 
 app.get('/', (req, res) => {
   res.send('Hola. Your default endpoint is working');
 });
 
-// app.get('/pets', findPets);
+app.get('/pets', handlePets);
+
+
 
 
 
